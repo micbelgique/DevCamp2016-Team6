1bc5e8dc54391748e030edd328bce01d72b3ffc2
Aurélien Malisart <aurelien.malisart@gmail.com>
2016-08-05 16:55:38 +0200

    deviceID

--- README.md
+++ README.md
@@ -6,3 +6,18 @@ Equipe: Creative Monkeys
 + François Stephany
 + ~~Julien Fontaine (Papa)~~
 + Michaël Hoste
+
+## Backend
+
+    $ cd backend
+    $ bundle
+    $ rake app:reset
+    $ rails server
+
+## Mobile
+
+    $ cd mobile
+    $ npm i
+    $ react-native start
+    $ react-native run-ios
+    $ react-native run-android
--- backend/app/controllers/api/base_controller.rb
+++ backend/app/controllers/api/base_controller.rb
@@ -3,7 +3,7 @@ class Api::BaseController < ApplicationController
   before_action :find_or_create_user
 
   def find_or_create_user
-    device_id = request.headers['device_id'].to_s.strip
+    device_id = params['device_id'].to_s.strip
 
     if device_id.present?
       @user = User.where(device_id: device_id).first_or_create

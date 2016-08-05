b09cb60fa2400334f70446c1d33cf838af2fd7c9
Aurélien Malisart <aurelien.malisart@gmail.com>
2016-08-05 17:22:08 +0200

    admin

--- backend/app/controllers/admin/missions_controller.rb
+++ backend/app/controllers/admin/missions_controller.rb
@@ -2,4 +2,39 @@ class Admin::MissionsController < Admin::BaseController
   def index
     @missions = Mission.order(:name)
   end
+
+  def new
+    @mission = Mission.new
+    render 'form'
+  end
+
+  def create
+    @mission = Mission.new(strong_params)
+
+    if @mission.save
+      redirect_to admin_missions_path
+    else
+      render 'form'
+    end
+  end
+
+  def edit
+    @mission = Mission.find(params[:id])
+  end
+
+  def update
+    @mission = Mission.find(params[:id])
+
+    if @mission.update_attributes(strong_params)
+      redirect_to admin_missions_path
+    else
+      render 'form'
+    end
+  end
+
+  def destroy
+    @mission = Mission.find(params[:id])
+    @mission.destroy
+    redirect_to admin_missions_path
+  end
 end
--- backend/app/helpers/application_helper.rb
+++ backend/app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def fa(name, suffix = nil)
+    if suffix.present?
+      "#{content_tag(:i, '', :class => "fa fa-#{name}")}&nbsp;#{suffix}".html_safe
+    else
+      content_tag(:i, '', :class => "fa fa-#{name}")
+    end
+  end
 end

84174e509064087cf8b3c0aa1ad358541020bcdf
Aurélien Malisart <aurelien.malisart@gmail.com>
2016-08-06 10:02:11 +0200

    manual base64

--- backend/app/controllers/api/user_spot_links_controller.rb
+++ backend/app/controllers/api/user_spot_links_controller.rb
@@ -5,18 +5,19 @@ class Api::UserSpotLinksController < Api::BaseController
       @spot           = @mission.spots.find(params[:spot_id])
       @user_spot_link = @spot.user_spot_links.where(:user_id => @user.id).first_or_initialize
 
-      data = "data:image/jpg;base64,(#{strong_params[:picture]})"
+      tmp_path = "#{Rails.root}/tmp/#{UUIDTools::UUID.random_create}.pdf"
+      data     = Base64.decode64(params[:user_spot_link][:picture])
 
-      @user_spot_link.picture = data
-      @user_spot_link.save!
+      File.open(tmp_path, 'b') do |file|
+        file.write(data)
+      end
+
+      # @user_spot_link.picture = data
+      # @user_spot_link.save!
+
+      # FileUtils.rm(tmp_path)
     end
 
     render :nothing => true
-  end
-
-  private
-
-  def strong_params
-    params.require(:user_spot_link).permit(:picture)
   end
 end
--- backend/app/models/user_spot_link.rb
+++ backend/app/models/user_spot_link.rb
@@ -7,6 +7,6 @@ class UserSpotLink < ApplicationRecord
 
   # CarrierWave
 
-  mount_uploader :picture, PictureUploader, :file_name => 'picture'
+  mount_uploader :picture, PictureUploader
 
 end

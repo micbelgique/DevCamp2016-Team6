9b0bab2c9f0a8c60764ed328eca78f42f9f28ed1
Aurélien Malisart <aurelien.malisart@gmail.com>
2016-08-06 09:56:02 +0200

    CSRF

--- backend/app/controllers/api/user_spot_links_controller.rb
+++ backend/app/controllers/api/user_spot_links_controller.rb
@@ -5,7 +5,7 @@ class Api::UserSpotLinksController < Api::BaseController
       @spot           = @mission.spots.find(params[:spot_id])
       @user_spot_link = @spot.user_spot_links.where(:user_id => @user.id).first_or_initialize
 
-      data = "data:image/jpg;base64,#{strong_params[:picture]}"
+      data = "data:image/jpg;base64,(#{strong_params[:picture]})"
 
       @user_spot_link.picture = data
       @user_spot_link.save!

fa690f5b546707e1140ccd8aaf46181a009d89d2
Aurélien Malisart <aurelien.malisart@gmail.com>
2016-08-07 10:14:27 +0200

    user_spot_link remove

--- backend/app/controllers/api/user_spot_links_controller.rb
+++ backend/app/controllers/api/user_spot_links_controller.rb
@@ -6,6 +6,8 @@ class Api::UserSpotLinksController < Api::BaseController
       @user_spot_link = @spot.user_spot_links.where(:user_id => @user.id).first_or_initialize
 
       if params[:user_spot_link][:picture]
+        Rails.logger.info "je suis dans le if"
+
         tmp_path = "#{Rails.root}/tmp/#{UUIDTools::UUID.random_create}.jpg"
         data     = Base64.decode64(params[:user_spot_link][:picture])
 
@@ -19,6 +21,7 @@ class Api::UserSpotLinksController < Api::BaseController
         @user_spot_link.save!
         FileUtils.rm(tmp_path)
       else
+        Rails.logger.info "je suis dans le else"
         @user_spot_link.destroy! if @user_spot_link.persisted?
       end
     end
